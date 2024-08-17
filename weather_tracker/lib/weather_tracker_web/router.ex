064ed6b9c458a7140d18b2cc13641148a6b70bb7
Oliver Pile <oliver.pile443@gmail.com>
2024-08-17 16:13:15 +0100

    Add API for creating entrys

--- weather_tracker/lib/weather_tracker_web/router.ex
+++ weather_tracker/lib/weather_tracker_web/router.ex
@@ -7,15 +7,9 @@ defmodule WeatherTrackerWeb.Router do
 
   scope "/api", WeatherTrackerWeb do
     pipe_through :api
+
+    post "/weather-conditions", WeatherConditionsController, :create
   end
 
-  # Enable Swoosh mailbox preview in development
-  if Application.compile_env(:weather_tracker, :dev_routes) do
 
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
 end
