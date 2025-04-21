f81ee360b6c29e42fd8e5f09e66cdc423bf102cf
Oliver Pile <oliver.pile443@gmail.com>
2025-04-21 18:13:17 +0100

    Update api ip address

--- sensor_hub/config/target.exs
+++ sensor_hub/config/target.exs
@@ -94,5 +94,4 @@ config :mdns_lite,
 
 # import_config "#{Mix.target()}.exs"
 
-config :sensor_hub, :weather_tracker_url,
-  "http://192.168.1.107:4000/api/weather-conditions"
+config :sensor_hub, :weather_tracker_url, "http://192.168.1.226:4000/api/weather-conditions"
